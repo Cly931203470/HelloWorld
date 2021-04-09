14d7195f608eaebc91d8019c0e971384ed641300
cly <931203470@qq.com>
2021-04-09 10:57:49 +0800

    version.1

--- src/com/cly/HelloWorld.java
+++ src/com/cly/HelloWorld.java
@@ -3,5 +3,6 @@ package com.cly;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("这是我的第一个上传项目");
+        System.out.println("这是我项目尊贵v2版本");
     }
 }
